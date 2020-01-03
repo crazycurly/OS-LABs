f3ac7b14b0ea51f7128db1dcd8ab5118eae4a09d
crazycurly <sam60317@gmail.com>
2020-01-03 20:02:31 +0800

    Simplify code

--- lab13/ex1.c
+++ lab13/ex1.c
@@ -9,23 +9,20 @@ pthread_mutex_t mtx;
 static void *threadFunc_add(void *arg){
 	int loops = *((int *)arg);
 	int loc,j,s;
+	printf("loops = %d\n",loops);
 
 	s = pthread_mutex_init(&mtx,NULL);
 	for (int i = 0; i < loops; ++i)
 	{
 		s = pthread_mutex_lock(&mtx);
-		if(s != 0)
-			printf("pthread_mutex_lock ERROR");
 
 		loc = glob ;
 		loc+=2;
 		glob = loc;
+		printf("G+ = %d\n",glob);
+
 
 		s = pthread_mutex_unlock(&mtx);
-
-		if(s != 0)
-			printf("pthread_mutex_lock ERROR");
-
 	}
 	return NULL;
 }
@@ -33,23 +30,19 @@ static void *threadFunc_add(void *arg){
 static void *threadFunc_sub(void *arg){
 	int loops = *((int *)arg);
 	int loc,j,s;
+	printf("loops = %d\n",loops);
 
 	s = pthread_mutex_init(&mtx,NULL);
 	for (int i = 0; i < loops; ++i)
 	{
 		s = pthread_mutex_lock(&mtx);
-		if(s != 0)
-			printf("pthread_mutex_lock ERROR");
 
 		loc = glob ;
 		loc-=2;
 		glob = loc;
+		printf("G- = %d\n",glob);
 
 		s = pthread_mutex_unlock(&mtx);
-
-		if(s != 0)
-			printf("pthread_mutex_lock ERROR");
-
 	}
 	return NULL;
 }
@@ -62,38 +55,16 @@ int main(int argc, char  *argv[])
 	loops = atoi(argv[1]);
 
 	s = pthread_create(&t1,NULL,threadFunc_add,&loops);
-
-	if(s != 0)
-		printf("pthread_create ERROR");
-
 	s = pthread_create(&t2,NULL,threadFunc_add,&loops);
-
-	if(s != 0)
-		printf("pthread_create ERROR");
-
 	s = pthread_create(&t3,NULL,threadFunc_sub,&loops);
-
-	if(s != 0)
-		printf("pthread_create ERROR");
+	
 
 	s = pthread_join(t1,NULL);
-
-	if(s != 0)
-		printf("pthread_join ERROR");
-
 	s = pthread_join(t2,NULL);
-
-	if(s != 0)
-		printf("pthread_join ERROR");
-
 	s = pthread_join(t3,NULL);
-
-	if(s != 0)
-		printf("pthread_join ERROR");
 
 	printf("glob = %d\n",glob);
 
 	s = pthread_mutex_destroy(&mtx);
- 
-	return 0;
+    return 0;
 }
